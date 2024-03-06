df98f76105a18ccb011a9ce9f8827d551472a114
siweli <lewisr.py@gmail.com>
2024-03-06 15:52:08 +0000

    generate unique token to identify device and receive otp code for registration

---
+++ api req test.py
@@ -0,0 +1,17 @@
+#& ".venv/Scripts/Activate.ps1"
+
+import requests
+from uuid import getnode
+import re
+
+mac_add = str(getnode())
+print(mac_add)
+
+url = "http://localhost:3000/api/regdevice"
+data = {"data" : mac_add}
+
+try:
+    reponse = requests.post(url, json=data)
+    print(reponse.json())
+except requests.ConnectionError:
+    print("Could not establish a connection")
